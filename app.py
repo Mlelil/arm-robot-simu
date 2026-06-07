dba808560c290dd754d9a1a2cda74c9eb9c1a6d2
Emile Sassine <sassine.emile@gmail.com>
2026-06-07 18:10:39 +0200

    refactor(App) Création et Organisation des fonctions de la classe App

--- app.py
+++ app.py
@@ -4,23 +4,65 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from robot import Robot
+from controller import Controller
+from history import MotionHistory
 
 class App:
-    def __init__(self):
-        self.root = tk.Tk()
-        self.root.title("Simulateur de Bras Robotique v3")
-        self.root.geometry("1100x750")
+    def __init__(self, robot: Robot, controller: Controller, history: MotionHistory):
+        self.robot = robot
+        self.controller = controller
+        self.history = history
 
-        self.style = ttk.Style()
-        self.style.theme_use('clam')
-        self.style.configure('Title.TLabel', font=('Helvetica', 12, 'bold', 'underline'))
-        self.style.configure('Data.TLabel', font=('Consolad', 9), background='#dddddd', padding=5)
+        # États GUI
+        self.is_recording = False # par exemple
 
-        # Conteneur de gauche (contrôles, largeur fixe)
-        self.sidebar = 3
+        # Construction dans l'ordre
+        self._build_window()
+        self._build_canvas()
+        self._build_sidebar()
+        self._draw_robot(controller.q_state)
+        
+    
+    # --- Dessin ---
+    def _draw_robot(self, q):
+        pass
 
-        # Conteneur de droite : Simulation 3D
+    # --- Callbacks Slider ---
+    def _update_data_from_sliders(self, *args):
+        pass
 
-        #etc...
+    # --- Boucle auto ---
+    def _auto_step(self):
+        pass
 
+    # --- Callbacks Boutons ---
+    def _apply_auto_command(self):
+        pass
+    def _draw_trajectory(self):
+        pass
+    def _clear_trajectory(self):
+        pass
+    def _toggle_recording(self):
+        pass
+    def _pause_recording(self):
+        pass
+    def _open_analysis_window(self):
+        pass
+    def _reset_robot(self):
+        pass
+    def _emergency_stop(self):
+        pass
+    
+    # --- Builders privés ---
+    def _build_window(self): # root, styles
+        pass
+    def _build_canvas(self): # fig, axes, canvas matplotlib
+        pass
+    def _build_sidebar(self): # widgets tkinter
+        pass
+
+    # --- Run ---
+    def run(self):
+        self.root.mainloop()
 
